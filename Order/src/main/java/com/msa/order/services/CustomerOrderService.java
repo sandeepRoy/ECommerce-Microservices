cf923bc971014ba6c4a05ca5959b1aca6b1f2db9
sandeepRoy <sandeep.roy2014@gmail.com>
2024-11-10 19:20:26 +0530

    Commit - 6
    Secret Key Removal :)

--- Order/src/main/java/com/msa/order/services/CustomerOrderService.java
+++ Order/src/main/java/com/msa/order/services/CustomerOrderService.java
@@ -17,8 +17,6 @@ public class CustomerOrderService {
 
     public static final Logger logger = Logger.getLogger(CustomerOrderService.class.getName());
 
-    public static final String SECRET_KEY = "cKNpYuq49z28DN+sH1FpDVLWX4vMd12QGWHx62oj3BGrQ4uNCr4Yxm5St3/P5dMUgVt3aK/0BK+zuqnQHMYZ1xAUMTpV09YCXimbAP2SYkUlZqI1XbIT5Idxsdu41xZ+VZAH3h6EZ+2WdrzezxJJ30URiyHu7bgGMPwoQjxidd5HR0uv7BVhD9xxEkI4jgWaZl0i9uKAZSqFfTaCTKCUzbK/COBQbj1SUQ7qT30XBTSdla+lK04wLAaJeiyGoXxnNFfMlS20uzmBJba8AdHxpmMmajptR8BdAUf+2HaX2MSHCzZRHXNwuW7mxFbDrMl0JpCAABBSMd7E51GaDnA1Vjcao7rzFuLVCXzkNt8P4F4";
-
     @Autowired
     public OrderRepository orderRepository;
 
